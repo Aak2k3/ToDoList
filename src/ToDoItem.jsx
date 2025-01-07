a4fabb4a916a765a83a19301bb0f6a0bbb813d5e
Aak2k3 <me.aakash@gmail.com>
2025-01-07 10:42:10 +0530

    added delete button

--- src/ToDoItem.jsx
+++ src/ToDoItem.jsx
@@ -1,8 +1,26 @@
 
 
-function ToDoItem(){
+function ToDoItem(props) {
 
-    console.log("yaay")
+    function deleteField(e){
+        console.log(e.target);
+        props.deleteButton();
+    }
+     
+
+
+    return (
+        <div className="inputText">
+            <div className="A">{props.text}</div>
+            <div className="B"><input className="checkbox" type="checkbox" /></div>
+            <div className="C">
+                <div className="delete"  onClick={deleteField}><img src="https://img.icons8.com/?size=100&id=67884&format=png&color=000000" alt="" width="20px" /></div>
+                <div className="edit"><img src="https://img.icons8.com/?size=100&id=71201&format=png&color=000000" alt="" width="20px" /></div>
+            </div>
+        </div>
+    );
 }
 
 export default ToDoItem;
+
+
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,11 +3,15 @@ import ToDoItem from "./ToDoItem";
 
 function ToDoList(){
     const [inputVal, setInputVal] = useState('');
+    const [todoItems, setTodoItems] = useState([]);
 
     
 
     function add(){
-        console.log(inputVal)
+        if (inputVal.trim() !== "") {
+            setTodoItems([...todoItems, inputVal]);  
+            setInputVal(''); 
+        }
         
     }
 
@@ -15,17 +19,30 @@ function ToDoList(){
         setInputVal(e.target.value)
     }
 
+    function deleteField(index) {
+        const updatedItems = todoItems.filter((item, id) => id !== index); 
+        setTodoItems(updatedItems); 
+    }
+
     return(
         <>
         <div className="input">
-            <input type="text" className="text" onChange={inputText} />
+            <input type="text" className="text" onChange={inputText} value={inputVal} />
             <button className="button" onClick={add}>ADD</button>
         </div>
         <div className="table">
             <div className="to-do">To DO</div>
-            <div className="completed">Completed?</div>
+            <div className="completed">Done?</div>
             <div className="del">Delete / Edit</div>
+            
 
+        </div>
+        <div>
+        {
+                todoItems.map((item, index)=>(
+                    <ToDoItem key={index} text={item} deleteButton={() => deleteField(index)} />
+                ))
+            }
         </div>
         </>
     )
